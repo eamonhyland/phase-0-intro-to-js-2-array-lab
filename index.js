41b3b24a29449b081568d89a2520cc64d3770041
Eamon <esh2464@email.vccs.edu>
2022-10-17 11:52:18 -0400

    commit tree fe1dd9e6ea7ca828507d46757f988be7f4442733

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"]; 
+
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+} 
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat() {
+    const newCats = [...cats,"Broom"];
+return newCats;
+}
+
+function prependCat() {
+    const afterstupidCats = ["Arnold", ...cats];
+return afterstupidCats;
+}
+
+function removeLastCat() {
+   const sliceStupidCats = cats.slice(0,-1);
+return sliceStupidCats;
+}
+
+function removeFirstCat() {
+    const plugAnnoyingCat = cats.slice(1);
+return plugAnnoyingCat;
+}
